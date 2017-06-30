b3e64b5a16ef1865eb2f81e4276392c9516022cc
Put the finger on the trigger <henrik.seiffert@gmail.com>
2017-06-30 16:57:03 +0200

    25

--- testpackage/application.py
+++ testpackage/application.py
@@ -39,7 +39,7 @@ from pathlib import Path
 ## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 ## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
 ## $QT_END_LICENSE$
-##
+## 25
 #############################################################################
 
 
